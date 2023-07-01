1ea2494977e79cdbbefe062bcb7c54d06333f10c
unknown <nourmostafa01551@gmail.com>
2023-07-01 03:35:55 +0300

    edit latest work section

--- arabic/js/script.js
+++ arabic/js/script.js
@@ -20,3 +20,19 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+function openTap(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "inline";
+  evt.currentTarget.className += " active";
+}
--- js/script.js
+++ js/script.js
@@ -20,3 +20,39 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+//tabs
+
+function openTap(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+  
+}// Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+
+
+
+  //show more
+  function showMore() {
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (moreText.style.display === "block") {
+      btnText.innerHTML = "VIEW MORE WORKS";
+      moreText.style.display = "none";
+    } else {
+      btnText.innerHTML = "VIEW LESS WORKS";
+      moreText.style.display = "block";
+    }
+  }
